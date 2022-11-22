992195533ea372aa2af95b18c2cc1fe0b96d3197
Mario González <mariogonzalez@iMac-de-Mario.local>
2022-11-22 14:33:17 -0600

    update config db

--- config/database.js
+++ config/database.js
@@ -2,11 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'containers-us-west-21.railway.app'),
-      port: env.int('DATABASE_PORT', 7190),
-      database: env('DATABASE_NAME', 'railway'),
+      host: env('DATABASE_HOST', 'db.asdvnafyxeylheulkmrk.supabase.co'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'postgres'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'jxNQxnE0OXu6Zb33eGuq'),
+      password: env('DATABASE_PASSWORD', 'U0CJMUoYQLrbLtaf'),
+      //U0CJMUoYQLrbLtaf
       schema: env('DATABASE_SCHEMA', 'public'),
      // ssl: env.bool('DATABASE_SSL', true),
       ssl: {
